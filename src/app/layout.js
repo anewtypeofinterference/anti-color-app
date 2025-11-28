1a6fbf76d001f8d37b05e9e2daa8494dbda3d948
Morten Johansen <morten@anti.as>
2025-11-28 10:28:02 +0100

    misc: auth and print fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="no">
+      
       <body>
         <AuthProvider>
           {/* this will redirect to /auth/signin if you're not logged in */}
